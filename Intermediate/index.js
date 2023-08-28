e3cc61b1c717c5e8154f2e5d10920426cc523689
prakashAd <adhikariprakash992@gmail.com>
2023-08-28 08:07:58 +0545

    switch added

--- Intermediate/index.js
+++ Intermediate/index.js
@@ -346,3 +346,35 @@
 //   //error handling
 // }
 
+
+
+
+//switch statements
+
+// let day = new Date().getDay();
+
+// switch (day) {
+//   case 0:
+//     console.log("Sunday");
+//     break;
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   default:
+//     console.log("Unknown day");
+// }
